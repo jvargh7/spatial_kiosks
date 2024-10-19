1c42e63321cf8851efc6c2006250e7c67ca52f1e
Thomas Hsiao <thsiao3@emory.edu>
2024-10-19 00:19:15 -0400

    removal cleanup

--- analyses/compute_urban_rucc.R
+++
@@ -1,9 +0,0 @@
-library(data.table)
-library(here)
-library(readxl)
-
-# Create urban/rural status for each county in the RUCC
-rucc <- data.table( read_excel(here("data", "Ruralurbancontinuumcodes2023.xlsx")) )
-rucc[, urban := ifelse(RUCC_2023 %in% 1:3, 1, 0)]
-
-fwrite(rucc, here("data", "reference", "RUCC2023.csv"))
